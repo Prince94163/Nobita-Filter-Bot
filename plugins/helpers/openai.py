bea90a06c3716b4d9c7e4a98719bf153f2ffa1af
Prince94163 <128693609+Prince94163@users.noreply.github.com>
2024-03-01 21:22:58 +0530

    Update openai.py

--- plugins/helpers/openai.py
+++ plugins/helpers/openai.py
@@ -1,9 +1,9 @@
 from info import S_GROUP, OPENAI_API
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from openai import OpenAI
+import openai
 
-ai_client = OpenAI(api_key=OPENAI_API)
+ai_client = OPENAI_API
 
 @Client.on_message(filters.command("openai"))
 async def ask_question(client, message):
